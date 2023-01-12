195505b12e64f38ad9edb6064f067112ea31ab57
Shaun Carr <shaunc138@gmail.com>
2023-01-12 01:24:21 -0500

    setting up auth

---
+++ lib/spotify.ts
@@ -0,0 +1,18 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+const scopes = ["user-read-email", "user-read-private", "streaming"].join(",");
+
+const params = {
+  scope: scopes,
+};
+
+const queryParamString = new URLSearchParams(params);
+
+export const LOGIN_URL = `https://accounts.spotify.com/authorize?${queryParamString.toString()}`;
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
+});
+
+export default spotifyApi;
