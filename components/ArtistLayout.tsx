b7a03f1dd070f81882f07dc825803b366b53c67a
Shaun Carr <shaunc138@gmail.com>
2023-02-21 12:13:50 -0500

    building ui of album layout

--- components/ArtistLayout.tsx
+++ components/ArtistLayout.tsx
@@ -1,40 +1,57 @@
 import { Session } from "next-auth";
 import useSpotify from "../hooks/useSpotify";
-import { useEffect } from "react";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { searchSelectedArtistState } from "../atoms/searchSelectedArtist";
-import {
-  artistComponentOpenState,
-  selectedArtistId,
-} from "../atoms/artistAtom";
+import { selectedArtistId } from "../atoms/artistAtom";
+import Image from "next/image";
 
 interface Props {
   session: Session | null;
 }
 
 export default function ArtistLayout({ session }: Props) {
+  const [albums, setAlbums] = useState<SpotifyApi.AlbumObjectSimplified[]>([]);
   const artistId = useRecoilValue(selectedArtistId);
-  const [artist, setArtist] = useRecoilState(searchSelectedArtistState);
-  const setArtistComponentOpen = useSetRecoilState(artistComponentOpenState);
+  // const [artist, setArtist] = useRecoilState(searchSelectedArtistState);
+  const [artist, setArtist] = useState<SpotifyApi.SingleArtistResponse | null>(
+    null
+  );
   const spotifyApi = useSpotify(session);
 
   useEffect(() => {
     if (spotifyApi.getAccessToken() && artistId) {
       spotifyApi.getArtist(artistId).then((data) => {
         setArtist(data.body);
+        console.log(data.body);
       });
     }
   }, [spotifyApi, setArtist, artistId]);
 
+  useEffect(() => {
+    spotifyApi.getArtistAlbums(artistId).then((data) => {
+      setAlbums(data.body.items);
+      console.log(data.body);
+    });
+  }, [spotifyApi, artistId]);
+
   return (
-    <div className="flex justify-center items-center h-full w-full pb-36 text-white">
-      <h1 className="pr-10">{artist.name}</h1>
-      <button
-        onClick={() => setArtistComponentOpen(false)}
-        className="text-gray-500 hover:text-white"
-      >
-        close
-      </button>
+    <div className="px-8">
+      <>
+        <div className="flex justify-start items-end space-x-5">
+          {artist && (
+            <Image
+              alt={`${artist?.name} image`}
+              src={artist?.images[2].url as string}
+              height={artist?.images[2].height}
+              width={artist?.images[2].width}
+            />
+          )}
+          <h1 className="pr-10 font-bold text-5xl">{artist?.name}</h1>
+        </div>
+        {albums &&
+          albums.map((album) => <div key={album.id}>{album.name}</div>)}
+      </>
     </div>
   );
 }
